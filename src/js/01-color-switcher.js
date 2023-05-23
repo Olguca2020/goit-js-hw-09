1c56008016d2fc82af54ae19b11ac2e17b90f75e
Olguca2020 <121376394+Olguca2020@users.noreply.github.com>
2023-05-24 00:58:28 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const refs = {
+  buttonStart: document.querySelector(`button[data-start]`),
+    buttonStop: document.querySelector(`button[data-stop]`),
+    body: document.querySelector(`body`),
+};
+let setIntervalId = null;
+const defaultInterval = 1000;
+refs.buttonStart.addEventListener(`click`, () => {
+    setIntervalId = setInterval(changColor, defaultInterval);
+    refs.buttonStart.disabled = true;
+    refs.buttonStop.disabled = false;
+});
+function changColor() { refs.body.style.backgroundColor = getRandomHexColor() };
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+refs.buttonStop.addEventListener(`click`, () => {
+    clearInterval(setIntervalId);
+    refs.buttonStart.disabled = false;
+    refs.buttonStop.disabled = true;
+})
 
